506ec1a6c3f30753c9116fddcd0f4a0d03a70732
Emilio Toledo <github@emiliotoledo.me>
2024-02-16 22:11:47 -0500

    feat: add missing properties for unique index

--- src/modules/prisma/dmmf/unique_index.rs
+++ src/modules/prisma/dmmf/unique_index.rs
@@ -2,4 +2,7 @@ use serde::{Deserialize, Serialize};
 
 #[derive(Serialize, Deserialize, Debug)]
 #[serde(rename_all = "camelCase")]
-pub struct Index {}
+pub struct Index {
+    name: String,
+    fields: Vec<String>,
+}
