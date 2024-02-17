62edc2fc9ae0deafc0b3eb457e02ef7aeb3ffc30
Emilio Toledo <github@emiliotoledo.me>
2024-02-16 22:28:53 -0500

    feat: add debug trait to generator and manifest callbacks

--- src/modules/oxigen/generate_callback.rs
+++ src/modules/oxigen/generate_callback.rs
@@ -2,6 +2,7 @@ use crate::modules::prisma::{generator_options::Options, request::Request};
 
 pub type Callback = Option<fn(Params)>;
 
+#[derive(Debug)]
 pub struct Params<'a> {
     pub message: &'a Request,
     pub options: &'a Options,
--- src/modules/oxigen/manifest_callback.rs
+++ src/modules/oxigen/manifest_callback.rs
@@ -2,6 +2,7 @@ use crate::modules::prisma::{generator_config::Config, request::Request};
 
 pub type Callback = Option<fn(Params)>;
 
+#[derive(Debug)]
 pub struct Params<'a> {
     pub message: &'a Request,
     pub config: &'a Config,
