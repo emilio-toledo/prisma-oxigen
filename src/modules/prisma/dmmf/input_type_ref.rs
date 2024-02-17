ef45b5c44f29a332c99deb89f17b776882b955eb
Emilio Toledo <github@emiliotoledo.me>
2024-02-16 21:31:00 -0500

    refactor: simplfiy input type refs allowed locations

--- src/modules/prisma/dmmf/input_type_ref.rs
+++ src/modules/prisma/dmmf/input_type_ref.rs
@@ -2,11 +2,11 @@ use serde::{Deserialize, Serialize};
 
 use super::type_ref;
 
-pub type Ref = type_ref::Ref<InputTypeRefAllowedLocations>;
+pub type Ref = type_ref::Ref<AllowedLocations>;
 
 #[derive(Serialize, Deserialize, Debug)]
 #[serde(rename_all = "camelCase")]
-pub enum InputTypeRefAllowedLocations {
+pub enum AllowedLocations {
     Scalar,
     InputObjectTypes,
     EnumTypes,
